fcfe2899a85504acb14539e4a866c9cc72d9e0bd
Kacper <kacper.jedrzejewski@int.pl>
2021-02-14 21:24:44 +0100

    Add project files.

---
+++ SavingsPigs.Mobile/SavingsPigs.Mobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SavingsPigs.Mobile.ViewModels;
+using SavingsPigs.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SavingsPigs.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
